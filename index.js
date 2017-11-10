68c3de560e62eb2f38bc845b9c3357176edd77de
Jaymun723 <jaymun723@yahoo.com>
2017-11-10 15:56:07 +0100

    Version 0.1.2

--- index.js
+++ index.js
@@ -1,4 +1,68 @@
-// Exports
-exports.set = require('./src/set')
-exports.createEngine = require('./src/engine')
-exports.servBundle = require('./src/servBundle')
+var React = require('react')
+var ReactDOMServer = require('react-dom/server')
+var _escaperegexp = require('lodash.escaperegexp')
+var beautifyHTML = require('js-beautify').html
+var express = require('express')
+var browserify = require('browserify')
+var path = require('path')
+var router = express.Router()
+
+module.exports = {
+  set (arg, text) {
+    if (arg === 'bundleDir') {
+      router.all('/:name', function (req, res) {
+        browserify()
+          .add(path.join(`${text}`, `${req.params.name}.jsx`))
+          .transform('babelify', {presets: ['env', 'react']})
+          .bundle()
+          .pipe(res)
+      })
+    }
+  },
+
+  createEngine (engineOptions = {}) {
+    let registered = false
+    let moduleDetectRegEx
+
+    function renderFile (filename, options, callback) {
+      if (!moduleDetectRegEx) {
+        moduleDetectRegEx = new RegExp([].concat(options.settings.views).map(viewPath => '^' + _escaperegexp(viewPath)).join('|'))
+      }
+
+      if (!registered) {
+        require('babel-register')({
+          extensions: ['.jsx'],
+          presets: ['react', 'env']
+        })
+        registered = true
+      }
+
+      try {
+        let component = require(filename)
+        component = component.default || component
+        component = React.createElement(component, options)
+        var result = '<!DOCTYPE html>' + ReactDOMServer.renderToString(component)
+      } catch (error) {
+        callback(error, null)
+      } finally {
+        if (options.settings.env === 'development') {
+          Object.keys(require.cache).forEach(function (module) {
+            if (moduleDetectRegEx.test(require.cache[module].filename)) {
+              delete require.cache[module]
+            }
+          })
+        }
+
+        if (engineOptions.beautify) {
+          result = beautifyHTML(result)
+        }
+
+        callback(null, result)
+      }
+    }
+    return renderFile
+  },
+
+  servBundle: router
+
+}
--- src/set.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (arg, text) {
-  if (arg === 'bundleDir') {
-    bundleDir = text // eslint-disable-line no-global-assign
-  }
-}
